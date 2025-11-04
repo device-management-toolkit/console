0b40dc04ef9b6279d765ee97d8db9b3dc778a42f
Nabendu Maiti <nabendu.bikash.maiti@intel.com>
2025-11-04 04:56:18 +0000

    build: proxy settings for build and devcontainer
      -> devcontainer go version updated to 1.25
      -> devcontainer updated to include proxy settings
      -> build script support proxy build
    
    Signed-off-by: Nabendu Maiti <nabendu.bikash.maiti@intel.com>

--- build.sh
+++ build.sh
@@ -2,16 +2,24 @@
 
 # Get version from the first argument
 version=$1
+# Set proxy environment variables only if they have values
+[ -n "${HTTP_PROXY:-${http_proxy:-}}" ] && export HTTP_PROXY="${HTTP_PROXY:-${http_proxy:-}}"
+[ -n "${HTTPS_PROXY:-${https_proxy:-}}" ] && export HTTPS_PROXY="${HTTPS_PROXY:-${https_proxy:-}}"
+[ -n "${NO_PROXY:-${no_proxy:-}}" ] && export NO_PROXY="${NO_PROXY:-${no_proxy:-localhost,127.0.0.1}}"
 
 # Build Docker images for each variant
 # Full build (with UI)
-docker build -t vprodemo.azurecr.io/console:v$version \
-             -t vprodemo.azurecr.io/console:latest .
+docker build --build-arg HTTP_PROXY="$HTTP_PROXY" --build-arg HTTPS_PROXY="$HTTPS_PROXY" \
+            --build-arg NO_PROXY="$NO_PROXY" \
+            -t vprodemo.azurecr.io/console:v$version \
+            -t vprodemo.azurecr.io/console:latest .
 
 # Headless build (No UI)
-docker build --build-arg BUILD_TAGS="noui" \
-             -t vprodemo.azurecr.io/console:v$version-headless \
-             -t vprodemo.azurecr.io/console:latest-headless .
+docker build --build-arg --build-arg HTTP_PROXY="$HTTP_PROXY" \
+            --build-arg HTTPS_PROXY="$HTTPS_PROXY" --build-arg NO_PROXY="$NO_PROXY" \
+            --build-arg BUILD_TAGS="noui" \
+            -t vprodemo.azurecr.io/console:v$version-headless \
+            -t vprodemo.azurecr.io/console:latest-headless .
 
 # Mark the Unix system outputs as executable
 chmod +x dist/linux/console_linux_x64
