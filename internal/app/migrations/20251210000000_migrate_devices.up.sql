7b2ab85d00ace602d2dedc7e76dd72d464d6e1eb
Mike Johanson <michael.johanson@intel.com>
2026-05-05 08:46:00 -0700

    fix: ensure migrations are idempotent

--- internal/app/migrations/20251210000000_migrate_devices.up.sql
+++ internal/app/migrations/20251210000000_migrate_devices.up.sql
@@ -3,7 +3,7 @@
 * SPDX-License-Identifier: Apache-2.0
 **********************************************************************/
 
-ALTER TABLE devices ADD COLUMN mebxpassword TEXT;
-ALTER TABLE devices ADD COLUMN mpspassword TEXT;
+ALTER TABLE devices ADD COLUMN IF NOT EXISTS mebxpassword TEXT;
+ALTER TABLE devices ADD COLUMN IF NOT EXISTS mpspassword TEXT;
 
-ALTER TABLE ciraconfigs ADD COLUMN generate_random_password TEXT;
+ALTER TABLE ciraconfigs ADD COLUMN IF NOT EXISTS generate_random_password TEXT;
