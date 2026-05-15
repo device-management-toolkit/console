18ba76dfb6c41f5e394f9ba31de9583be5f48dd3
Madhavi Losetty <madhavi.losetty@intel.com>
2026-05-15 11:10:22 -0700

    fix(api): allow profile PATCH without passwords (#973)

--- internal/entity/dto/v1/canceled.go
+++ internal/entity/dto/v1/canceled.go
@@ -12,8 +12,8 @@ func (e CanceledError) Error() string {
 	return e.Console.Error()
 }
 
-func (e CanceledError) Wrap(function, call string, err error) error {
-	_ = e.Console.Wrap(function, call, err)
+func (e CanceledError) Wrap(call, function string, err error) error {
+	_ = e.Console.Wrap(call, function, err)
 	e.Console.Message = "Canceled: " + err.Error()
 
 	return e
